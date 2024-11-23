c484afaa4317346aab28146863e47479cfbdc2f1
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2024-11-23 18:37:22 +0530

    deployment process

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,3 +1,3 @@
-const isDevelopment = 'development';
+const isDevelopment =false;
 export const URL = isDevelopment ? 'http://localhost:3000/api/v1' : '/api/v1';
 export const clientURL = isDevelopment ? 'http://localhost:5176' : '';
