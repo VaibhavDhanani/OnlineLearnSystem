cc32940d5c4730206e5d8151cac8c55d91268859
Vaibhav Dhanani <130571437+VaibhavDhanani@users.noreply.github.com>
2025-07-12 03:16:22 +0000

    removed unnecessary code

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -4,7 +4,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 export const URL = isDevelopment ? 'http://localhost:3000/api/v1' : '/api/v1';
 // const JWT_KEY = JWT_SECRET_KEY;
 // export default JWT_KEY;
-export const EMAIL_USER="vaibhavdhanani2711@gmail.com"
-export const USER_NAME="OnlineLearn"
-export const EMAIL_PASS="hbvaiakcqbxfttuv"
+export const EMAIL_USER=""
+export const USER_NAME=""
+export const EMAIL_PASS=""
 
